ab7164b00eab5c3ad789d7baa50d29d0e2ff1bf6
issadlounis <issadcntsid@gmail.com>
2024-05-03 14:35:01 +0200

    lab22

--- src/main/java/dz/lab22/lab22/MaConfiguration.java
+++ src/main/java/dz/lab22/lab22/MaConfiguration.java
@@ -3,7 +3,9 @@ package dz.lab22.lab22;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,46 +14,34 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class MaConfiguration {
     @Bean
     public BCryptPasswordEncoder pwdEncoder() { return new BCryptPasswordEncoder(); }
-    @Bean public UserDetailsService userDetailsService(BCryptPasswordEncoder bcrypt) {
+    @Bean
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder bcrypt) {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("user")
                 .password(bcrypt.encode("123")).roles("USER").build());
-        manager.createUser(User.withUsername("admin")
-                .password(bcrypt.encode("987")).roles("USER", "ADMIN").build());
+        // manager.createUser(User.withUsername("admin")
+        //        .password(bcrypt.encode("987")).roles("USER", "ADMIN").build());
         return manager;
     }
-    @Bean 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 
         http.authorizeHttpRequests(request -> request
-                .requestMatchers("/index").permitAll()
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .requestMatchers("/portail/**").hasAnyAuthority("USER", "ADMIN")
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/").hasRole("USER")
+                // .requestMatchers("/admin/**").hasRole("ADMIN")
+                // .requestMatchers("/portail/**").hasAnyAuthority("USER", "ADMIN")
                 .anyRequest().authenticated()
         );
+        http.formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+        // http.logout(LogoutConfigurer::permitAll);
         http.exceptionHandling(exception -> exception.accessDeniedPage("/403"));
         return http.build();
     }
-
-    /*
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(request -> request
-                .requestMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-        );
-        http.formLogin(formLogin -> formLogin.loginPage("/login").permitAll());
-        http.logout(LogoutConfigurer::permitAll);
-        return http.build();
-    }*/
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**");
-    }
-
 }
 
